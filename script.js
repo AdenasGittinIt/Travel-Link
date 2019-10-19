064606b2d8a17a2c6712940ae4bb550132bde40e
Tyson Call <Tyson@Savannahs-MacBook-Air.local>
2019-10-19 11:05:59 -0600

    d

--- script.js
+++ script.js
@@ -1,1 +1,22 @@
-// user input with their date range
+
+//Take user imput of date (Confirm that API keys can search by date and the format required)
+  //future dev... date range
+
+//onclick function that captures the user input and inserts it into the API request URL
+
+//AJAX function to send the request 
+
+//Get the results back from the API
+
+//dynamically updating the DOM with the search results
+  //each result should have a check box next to it
+
+//On click function that takes the checked event or restaurant and updates the intinerary with the event or restuarant
+
+//Extra Credit... create a modal that pops up with a message depending on what the user adds to their list
+  //"that looks delicious" for a restaurangt
+  //"don't forget to check the weather" if it's an outdoor event
+  //"that looks fun" for another envet
+  //"you have great taste in music" for a musical event
+
+
